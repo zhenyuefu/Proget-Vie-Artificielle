c5ce2ffd4bc46a6c9965ff2f18a9e76e9f91bc3e
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-02-19 18:38:29 +0100

    modif

--- myworld.py
+++ myworld.py
@@ -12,11 +12,13 @@ class World:
     classe principale du jeux
     """
 
-    def __init__(self, size_factor_X=40, size_factor_Y=20, size_tile_X=32, size_tile_Y=32):
+    def __init__(self, size_factor_X=50, size_factor_Y=28, size_tile_X=23, size_tile_Y=23):
         """
         constructeur de la classe
         size_factor_X et size_factor_Y représentent la taille du plateau de jeux en nombre de tuiles 64*64 pixels
         """
+
+
 
         self.size_factor_X, self.size_factor_Y = size_factor_X, size_factor_Y
 
@@ -24,34 +26,59 @@ class World:
 
         self.Case=[]
 
-        self.Map=[]
+        self.MountainsType=[]
 
-        for y in range(0,self.size_factor_Y):
+        self.Map_trees=[x[:] for x in [[0] * self.size_factor_X] * self.size_factor_Y]
 
-            L=[]
+        self.Map_obtacles=[x[:] for x in [[0] * self.size_factor_X] * self.size_factor_Y]
 
-            for x in range(0,self.size_factor_X):
+        self.Map_mountains=[x[:] for x in [[0] * self.size_factor_X] * self.size_factor_Y]
+
+        #Type de montagnes
+
+        self.Mountain1=[[1,1,1],
+                        [1,1,1],
+                        [1,1,1]]
+
+        self.Mountain2=[[1,1,1,1,1,1],
+                        [1,1,1,1,1,1]]
+
+        self.MountainsType.append(self.Mountain1)
+        self.MountainsType.append(self.Mountain2)
+
+        # Mountains random placement
+
+        self.x_offset = randint(0,self.size_factor_X-1)
+        self.y_offset = randint(0,self.size_factor_Y-1)
+        self.M = self.MountainsType[randint(0,len(self.MountainsType)-1)]
+
+        for x in range( len( self.M[0] ) ):
+
+            for y in range( len( self.M ) ):
+
+                self.Map_mountains[y+self.y_offset][x+self.x_offset]=1
+
+        #Trees random placement
+
+        for x in range(0,len(self.Map_trees[0])):
+
+            for y in range(0,len(self.Map_trees)):
 
                 cellule = Cellule(y,x)
 
                 self.Case.append(cellule)
 
-                p = randint(0,10)
+                if not self.Map_mountains[y][x]==1:
 
-                if p < 10 :
+                    p = randint(0,10)
 
-                    L.append(1)
+                    if p < 5 :
 
-                else:
+                        self.Map_trees[y][x]=1
 
-                    L.append(0)
+        #self.Map_trees[self.size_factor_Y//2][self.size_factor_X//2]=2
 
-            self.Map.append(L)
-
-
-        self.Map[self.size_factor_Y//2][self.size_factor_X//2]=2
-
-        # self.Map = [[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        # self.Map_trees = [[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         #     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         #     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         #     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -62,6 +89,25 @@ class World:
         #     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         #     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]]
 
+
+        # Obstacles random placement
+
+        for x in range(0,len(self.Map_trees[0])):
+
+            for y in range(0,len(self.Map_trees)):
+
+                if not y==self.size_tile_Y//2 and not x==self.size_factor_X//2:
+
+                    if not self.Map_trees[y][x]==1 and not self.Map_mountains[y][x]==1:
+
+                        p = randint(0,10)
+
+                        if p < 5 :
+
+                            self.Map_obtacles[y][x]=1
+
+        
+
     
         #self.size_X, self.size_Y = 128,128
 
@@ -70,9 +116,10 @@ class World:
 
         # self.scaleMultiplier_Y = self.size_tile_Y / self.size_Y
                                             
-        self.background_image_filename = 'dirt.png'   # image backgound 
-        self.tree_image_filename = 'treeSmall.png' 
-        self.burn_tree_image_filename = 'smokeOrange0.png'         
+        self.background_image_filename = 'topdowntanks/PNG/Environment/dirt.png'   # image backgound 
+        self.tree_image_filename = 'topdowntanks/PNG/Environment/treeSmall.png' 
+        self.burn_tree_image_filename = 'topdowntanks/PNG/Smoke/smokeOrange0.png' 
+        self.sandbag_image_filename = 'topdowntanks/PNG/Obstacles/sandbagBrown.png'        
         
 
         pygame.init()          
@@ -91,6 +138,9 @@ class World:
         self.burn_tree = pygame.image.load(self.burn_tree_image_filename).convert_alpha()
         self.burn_tree = pygame.transform.scale(self.burn_tree, (int(self.size_tile_X), int(self.size_tile_Y)))
 
+        self.sandbag = pygame.image.load(self.sandbag_image_filename).convert_alpha()
+        self.sandbag = pygame.transform.scale(self.sandbag, (int(self.size_tile_X), int(self.size_tile_Y)))
+
 
         self.Tmp = self.Case
 
@@ -106,7 +156,7 @@ class World:
         x = cell.getY()
         y = cell.getX()
 
-        if (self.Map[y][x]==2):
+        if (self.Map_trees[y][x]==2):
             for x2 in range(x-1,x+2):
                 for y2 in range(y-1,y+2):
                     x3 = x2
@@ -119,17 +169,17 @@ class World:
                         y3 += self.size_factor_Y
                     if (y3 >= self.size_factor_Y):
                         y3 -= self.size_factor_Y
-                    if (self.Map[y3][x3] == 1):
-                        self.Map[y3][x3] = 2
+                    if (self.Map_trees[y3][x3] == 1):
+                        self.Map_trees[y3][x3] = 2
 
-            self.Map[y][x] = 3
+            self.Map_trees[y][x] = 3
         
 
         
             
                 
 
-    def loop(self):
+    def stepWorld(self):
         """
         boucle de lecture infinie événementielles du jeux
 
@@ -147,14 +197,18 @@ class World:
             #affichage répété de la tuile de background en parcourant la fenêtre par pas de la taille des tuiles: 
             
             #for x in range(0, int(self.size_X*self.scaleMultiplier*self.size_factor_X), int(self.size_X*self.scaleMultiplier)):
-            for x in range (0,len(self.Map[0])):
+            for x in range (0,len(self.Map_trees[0])):
                 #for y in range(0, int(self.size_Y*self.scaleMultiplier*self.size_factor_Y), int(self.size_Y*self.scaleMultiplier)):
-                for y in range(0,len(self.Map)):
+                for y in range(0,len(self.Map_trees)):
                     self.screen.blit(self.background,(x*self.size_tile_X,y*self.size_tile_Y))  # tuile "background" en position (x,y)
-                    if (self.Map[y][x]==1):
-                        self.screen.blit(self.tree,(x*self.size_tile_X,y*self.size_tile_Y))
-                    if (self.Map[y][x]==2):
-                        self.screen.blit(self.burn_tree,(x*self.size_tile_X,y*self.size_tile_Y))
+                    # if (self.Map_trees[y][x]==1):
+                    #     self.screen.blit(self.tree,(x*self.size_tile_X,y*self.size_tile_Y))
+                    # if (self.Map_trees[y][x]==2):
+                    #     self.screen.blit(self.burn_tree,(x*self.size_tile_X,y*self.size_tile_Y))
+                    # if (self.Map_obtacles[y][x]==1):
+                    #     self.screen.blit(self.sandbag,(x*self.size_tile_X,y*self.size_tile_Y))
+                    if (self.Map_mountains[y][x]==1):
+                        self.screen.blit(self.sandbag,(x*self.size_tile_X,y*self.size_tile_Y))
                     
 
             pygame.display.update()                  
@@ -171,6 +225,6 @@ class World:
 if __name__ == '__main__':
     world=World()
     try:
-        world.loop()
+        world.stepWorld()
     except KeyboardInterrupt:  # interruption clavier CTRL-C: appel à la méthode destroy() de appl.
         world.destroy()
