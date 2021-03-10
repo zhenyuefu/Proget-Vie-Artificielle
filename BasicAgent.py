59f62f729422c0e567158dbb1106f9ecf35a73d3
ZhenyueFu <zhenyuef@gmail.com>
2021-03-11 00:28:05 +0100

    Animation in 4 directions

---
+++ BasicAgent.py
@@ -0,0 +1,58 @@
+import pygame  # PYGAME package
+
+
+class BasicAgent(pygame.sprite.Sprite):
+    def __init__(self, world, img, init_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.world = world
+        self.world_size_x = self.world.size_factor_X * self.world.size_tile_X
+        self.world_size_y = self.world.size_factor_Y * self.world.size_tile_Y
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = init_pos
+        self.speed = 5
+        self.frame = []
+        self.current_frame = 0
+        self.x = self.rect.x // self.world.size_tile_X
+        self.y = self.rect.y // self.world.size_tile_Y
+        self.direction = 3
+
+    def set_frame(self, frame):
+        self.frame = frame
+
+    def move(self):
+        self.current_frame += 1
+        if self.current_frame > 2:
+            self.current_frame = 0
+        self.image = self.frame[self.direction][self.current_frame]
+        self.x = self.rect.x // self.world.size_tile_X
+        self.y = self.rect.y // self.world.size_tile_Y
+        # [0] -> up
+        # [1] -> right
+        # [2] -> down
+        # [3] -> left
+        if self.direction == 0:
+            self.rect.y -= self.speed
+            if self.rect.bottom < 0:
+                self.rect.top = self.world_size_y
+            return
+
+        if self.direction == 1:
+            self.rect.x += self.speed
+            if self.rect.left > self.world_size_x:
+                self.rect.right = 0
+            return
+
+        if self.direction == 2:
+            self.rect.y += self.speed
+            if self.rect.top > self.world_size_y:
+                self.rect.bottom = 0
+            return
+
+        if self.direction == 3:
+            self.rect.x -= self.speed
+            if self.rect.right < 0:
+                self.rect.left = self.world_size_x
+
+    def update(self):
+        self.move()
--- test_animation.py
+++ test_animation.py
@@ -1,29 +1,10 @@
-from random import *
-import pygame  # PYGAME package
-from pygame.locals import *  # PYGAME constant & functions
 from sys import exit  # exit script
 
-class BasicAgent(pygame.sprite.Sprite):
-    def __init__(self,img,init_pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = img
-        self.rect = self.image.get_rect()
-        self.rect.midbottom = init_pos
-        self.speed = 5
-        self.frame = []
-        self.current_frame = 0
-    
-    def set_frame(self,frame):
-        self.frame = frame
-    
-    def move(self):
-        self.current_frame+=1
-        if self.current_frame > 2:
-            self.current_frame = 0
-        self.image = self.frame[self.current_frame]
-        self.rect.y += self.speed
-        if self.rect.y > 17*30:
-            self.rect.bottom = 0
+import pygame  # PYGAME package
+from pygame.locals import *  # PYGAME constant & functions
+
+import BasicAgent
+
 
 class World:
     """
@@ -31,7 +12,7 @@ class World:
     """
 
     def __init__(
-        self, size_factor_X=35, size_factor_Y=17, size_tile_X=30, size_tile_Y=30
+            self, size_factor_X=35, size_factor_Y=17, size_tile_X=30, size_tile_Y=30
     ):
         """
         constructeur de la classe
@@ -52,19 +33,20 @@ class World:
             DOUBLEBUF,
         )
         pygame.display.set_caption("WORLD TEST")
-        
-      
+
         self.Environment_images = []
-        self.Agent_images = []
-        self.loadAllImage()
-        self.agent = BasicAgent(self.Agent_images[0][0],(50,50))
-        self.agent.set_frame([self.Agent_images[0][0],self.Agent_images[0][1],self.Agent_images[0][2]])
+        self.sheep_images = []
+        self.load_all_image()
+        self.agent = BasicAgent.BasicAgent(self, self.sheep_images[0][0], (50, 50))
+        self.agent2 = BasicAgent.BasicAgent(self, self.sheep_images[0][0],(100,100))
+        self.agent2.direction = 1
+        self.agent2.set_frame(self.sheep_images)
+        self.agent.set_frame(self.sheep_images)
         self.agent_group = pygame.sprite.Group()
         self.agent_group.add(self.agent)
-        
-        
+        self.agent_group.add(self.agent2)
 
-    def loadImage(self, filename):
+    def load_image(self, filename):
 
         image = pygame.image.load(filename).convert_alpha()
 
@@ -74,64 +56,87 @@ class World:
 
         return image
 
-    def loadAllImage(self):
+    def load_all_image(self):
 
         # 0 : backgrounds
-        self.Environment_images.append([self.loadImage("dirt.png")])
+        self.Environment_images.append([self.load_image("dirt.png")])
 
         # 1 : trees
         self.Environment_images.append(
             [
-                self.loadImage("PNG/tree.png"),
-                self.loadImage("smokeOrange0.png"),
-                self.loadImage("PNG/wooded_tree.png"),
-                self.loadImage("PNG/big_tree.png"),
+                self.load_image("PNG/tree.png"),
+                self.load_image("smokeOrange0.png"),
+                self.load_image("PNG/wooded_tree.png"),
+                self.load_image("PNG/big_tree.png"),
             ]
         )
 
         # 2 : obstacles
         self.Environment_images.append(
-            [self.loadImage("PNG/rock1.png"), self.loadImage("PNG/rock2.png")]
+            [self.load_image("PNG/rock1.png"), self.load_image("PNG/rock2.png")]
         )
 
         # 3 : herbs
-        self.Environment_images.append([self.loadImage("grass.png")])
+        self.Environment_images.append([self.load_image("grass.png")])
 
         # 4 : Mountains
         self.Environment_images.append(
             [
-                self.loadImage("PNG/green_inside.png"),
-                self.loadImage("PNG/green_SW.png"),
-                self.loadImage("PNG/green_NW.png"),
-                self.loadImage("PNG/green_SE.png"),
-                self.loadImage("PNG/green_NE.png"),
-                self.loadImage("PNG/green_S.png"),
-                self.loadImage("PNG/green_N.png"),
-                self.loadImage("PNG/green_E.png"),
-                self.loadImage("PNG/green_W.png"),
-                self.loadImage("PNG/green_corner_NE.png"),
-                self.loadImage("PNG/green_corner_NW.png"),
-                self.loadImage("PNG/green_corner_SE.png"),
-                self.loadImage("PNG/green_corner_SW.png"),
+                self.load_image("PNG/green_inside.png"),
+                self.load_image("PNG/green_SW.png"),
+                self.load_image("PNG/green_NW.png"),
+                self.load_image("PNG/green_SE.png"),
+                self.load_image("PNG/green_NE.png"),
+                self.load_image("PNG/green_S.png"),
+                self.load_image("PNG/green_N.png"),
+                self.load_image("PNG/green_E.png"),
+                self.load_image("PNG/green_W.png"),
+                self.load_image("PNG/green_corner_NE.png"),
+                self.load_image("PNG/green_corner_NW.png"),
+                self.load_image("PNG/green_corner_SE.png"),
+                self.load_image("PNG/green_corner_SW.png"),
             ]
         )
 
-        # agents [0] : sheep
-        self.Agent_images.append(
+        # sheep_images[0] -> up
+        # sheep_images[1] -> right
+        # sheep_images[2] -> down
+        # sheep_images[3] -> left
+        self.sheep_images.append(
             [
-                self.loadImage("PNG/Agent/sheep/sheep_front_1.png"),
-                self.loadImage("PNG/Agent/sheep/sheep_front_2.png"),
-                self.loadImage("PNG/Agent/sheep/sheep_front_3.png"),
+                self.load_image("PNG/Agent/sheep/sheep_back_1.png"),
+                self.load_image("PNG/Agent/sheep/sheep_back_2.png"),
+                self.load_image("PNG/Agent/sheep/sheep_back_3.png"),
+            ]
+        )
+        self.sheep_images.append(
+            [
+                self.load_image("PNG/Agent/sheep/sheep_right_1.png"),
+                self.load_image("PNG/Agent/sheep/sheep_right_2.png"),
+                self.load_image("PNG/Agent/sheep/sheep_right_3.png"),
+            ]
+        )
+        self.sheep_images.append(
+            [
+                self.load_image("PNG/Agent/sheep/sheep_front_1.png"),
+                self.load_image("PNG/Agent/sheep/sheep_front_2.png"),
+                self.load_image("PNG/Agent/sheep/sheep_front_3.png"),
+            ]
+        )
+        self.sheep_images.append(
+            [
+                self.load_image("PNG/Agent/sheep/sheep_left_1.png"),
+                self.load_image("PNG/Agent/sheep/sheep_left_2.png"),
+                self.load_image("PNG/Agent/Sheep/sheep_left_3.png"),
             ]
         )
 
-    def updateWorld(self):
+    def update_world(self):
         """
         boucle de lecture infinie événementielles du jeux
 
         """
-        
-       
+
         # self.forestFire()
 
         # lecture des événements Pygame
@@ -141,13 +146,12 @@ class World:
         for x in range(0, self.size_factor_X):
             # for y in range(0, int(self.size_Y*self.scaleMultiplier*self.size_factor_Y), int(self.size_Y*self.scaleMultiplier)):
             for y in range(0, self.size_factor_Y):
-
                 self.screen.blit(
                     self.Environment_images[0][0],
                     (x * self.size_tile_X, y * self.size_tile_Y),
                 )  # tuile "background" en position (x,y)
-        
-        self.agent.move()
+
+        self.agent_group.update()
         self.agent_group.draw(self.screen)
         pygame.display.update()
 
@@ -163,10 +167,9 @@ class World:
 if __name__ == "__main__":
     world = World()
     clock = pygame.time.Clock()
-    a1 = BasicAgent(world.Agent_images[0][0],(1,1))
     while True:
         try:
-            world.updateWorld()
+            world.update_world()
         except KeyboardInterrupt:  # interruption clavier CTRL-C: appel à la méthode destroy() de appl.
             world.destroy()
         clock.tick(10)
