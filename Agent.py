a230781eaf0d1f17a1d054d65d1e424f136934e3
ZhenyueFu <zhenyuef@gmail.com>
2021-03-11 13:09:36 +0100

    rename class agent

--- Agent.py
+++ Agent.py
@@ -10,20 +10,26 @@ class BasicAgent(pygame.sprite.Sprite):
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.midbottom = init_pos
-        self.speed = 5
+        self.speed = 2
         self.frame = []
         self.current_frame = 0
         self.x = self.rect.x // self.world.size_tile_X
         self.y = self.rect.y // self.world.size_tile_Y
         self.direction = 3
+        self.alive = True
+        self.iter = 0
+        self.it_non_mange = 0
 
     def set_frame(self, frame):
         self.frame = frame
 
     def move(self):
-        self.current_frame += 1
-        if self.current_frame > 2:
-            self.current_frame = 0
+        self.iter += 1
+        if self.iter > 3:
+            self.current_frame += 1
+            if self.current_frame > 2:
+                self.current_frame = 0
+            self.iter = 0
         self.image = self.frame[self.direction][self.current_frame]
         self.x = self.rect.x // self.world.size_tile_X
         self.y = self.rect.y // self.world.size_tile_Y
@@ -56,3 +62,21 @@ class BasicAgent(pygame.sprite.Sprite):
 
     def update(self):
         self.move()
+
+
+class Sheep(BasicAgent):
+    p_reproduce = 0.00
+    delai_de_famine = 10
+
+    def __init__(self, world, img, init_pos):
+        super().__init__(world, img, init_pos)
+        self.frame = world.sheep_images
+
+
+class Wolf(BasicAgent):
+    p_reproduce = 0.00
+    delai_de_famine = 10
+
+    def __init__(self, world, img, init_pos):
+        super().__init__(world, img, init_pos)
+        self.frame = world.wolf_images
