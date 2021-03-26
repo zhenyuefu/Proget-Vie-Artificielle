ae24e1454d4dd9cd7785d3d52ff5d0d145ef4665
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-03-26 20:04:02 +0100

    weather_time

--- Weather.py
+++ Weather.py
@@ -1,6 +1,38 @@
-
+import pygame
 
 import random
+
+class Time:
+
+    def __init__(self):
+
+        self.hour = -1
+
+        self.time = 0
+
+    def update_hour(self):
+
+        self.time += 1
+
+        if self.time > 3:
+
+            self.time = 0
+
+            self.hour += 1
+            
+            if self.hour > 23:
+                
+                self.hour = 0
+                
+            print(self.hour)
+
+
+
+    def get_hour(self):
+
+        return self.hour
+
+
 
 
 class Weather:
@@ -9,9 +41,11 @@ class Weather:
 
         self.season = 0
 
+        self.heure = Time()
+
         self.temperature = 0
 
-        self.time = 0
+        self.delay = 0
 
 
     def reset_season(self):
@@ -44,7 +78,13 @@ class Weather:
 
         if self.season == 0:
 
-            self.temperature = random.uniform(float(25), float(30))
+            if self.heure.get_hour() > 6 and self.heure.get_hour() < 22:
+                
+                self.temperature = random.uniform(float(27), float(30))
+
+                return 
+
+            self.temperature = random.uniform(float(24), float(27))
 
             return
 
@@ -52,7 +92,13 @@ class Weather:
 
         if self.season == 1:
 
-            self.temperature = random.uniform(float(10), float(15))
+            if self.heure.get_hour() > 6 and self.heure.get_hour() < 20:
+                
+                self.temperature = random.uniform(float(13), float(16))
+
+                return
+
+            self.temperature = random.uniform(float(8), float(11))
 
             return
         
@@ -60,7 +106,13 @@ class Weather:
         
         if self.season == 2:
 
-            self.temperature = random.uniform(float(-10), float(-5))
+            if self.heure.get_hour() > 6 and self.heure.get_hour() < 20:
+                
+                self.temperature = random.uniform(float(-10), float(-6))
+
+                return
+
+            self.temperature = random.uniform(float(-15), float(-10))
 
             return
 
@@ -68,6 +120,23 @@ class Weather:
 
         if self.season == 3:
 
-            self.temperature = random.uniform(float(15), float(20))
+            if self.heure.get_hour() > 6 and self.heure.get_hour() < 21:
+                
+                self.temperature = random.uniform(float(15), float(20))
+
+                return
+
+            self.temperature = random.uniform(float(10), float(13))
 
             return
+
+    def update_weather(self):
+
+        self.update_season()
+        self.update_temperature()
+        self.heure.update_hour()
+
+
+
+
+
