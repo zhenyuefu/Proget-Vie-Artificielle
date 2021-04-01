eabaf37ebcf42c6b98b24a353957d03f9ee51fdd
ZhenyueFu <zhenyuef@gmail.com>
2021-04-01 09:46:05 +0200

    format

--- test_tree.py
+++ test_tree.py
@@ -20,16 +20,14 @@ class World:
     classe principale du jeux
     """
 
-    def __init__(
-            self, screenWidth=1476, screenHeight=1088
-    ):
+    def __init__(self, screenWidth=1476, screenHeight=1088):
         """
         constructeur de la classe
         """
 
         self.screenWidth, self.screenHeight = screenWidth, screenHeight
 
-        self.size_tile_X, self.size_tile_Y = 32,32
+        self.size_tile_X, self.size_tile_Y = 32, 32
 
         self.size_tree_X, self.size_tree_Y = 36, 32
 
@@ -37,20 +35,32 @@ class World:
 
         self.size_cloud_X, self.size_cloud_Y = 128, 128
 
-        self.size_grass_X, self.size_grass_Y = self.size_tree_X // 2, self.size_tree_Y // 2
+        self.size_grass_X, self.size_grass_Y = (
+            self.size_tree_X // 2,
+            self.size_tree_Y // 2,
+        )
 
         self.size_obstacle_X, self.size_obstacle_Y = self.size_tree_X, self.size_tree_Y
 
         self.size_block_X, self.size_block_Y = self.size_tree_X, self.size_tree_Y
 
-        self.Map_trees = [x[:] for x in
-                          [[None] * (self.screenWidth // self.size_tree_X)] * (self.screenHeight // self.size_tree_Y)]
+        self.Map_trees = [
+            x[:]
+            for x in [[None] * (self.screenWidth // self.size_tree_X)]
+            * (self.screenHeight // self.size_tree_Y)
+        ]
 
-        self.Map_grass = [x[:] for x in
-                          [[None] * (self.screenWidth // self.size_grass_X)] * (self.screenHeight // self.size_grass_Y)]
+        self.Map_grass = [
+            x[:]
+            for x in [[None] * (self.screenWidth // self.size_grass_X)]
+            * (self.screenHeight // self.size_grass_Y)
+        ]
 
-        self.Map_mountains = [x[:] for x in [[0] * (self.screenWidth // self.size_block_X)] * (
-                self.screenHeight // self.size_block_Y)]
+        self.Map_mountains = [
+            x[:]
+            for x in [[0] * (self.screenWidth // self.size_block_X)]
+            * (self.screenHeight // self.size_block_Y)
+        ]
 
         pygame.init()
 
@@ -109,29 +119,25 @@ class World:
 
         self.object_placment()
 
-        
-
-
-
     # Type de montagnes
 
     def create_mountain(self):
 
         for _ in range(3):
-
             longueur, largeur = random.randint(5, 8), random.randint(5, 8)
 
             self.MountainsType.append([x[:] for x in [[1] * largeur] * longueur])
 
-        self.MountainsType.append([[1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-                                    [1,2,2,2,2,2,2,2,2,2,2,2,2,1],
-                                    [1,2,3,3,3,3,3,3,3,3,3,3,2,1],
-                                    [1,2,3,3,3,3,3,3,3,3,3,3,2,1],
-                                    [1,2,2,2,2,2,2,2,2,2,2,2,2,1],
-                                    [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-
-        ])
-
+        self.MountainsType.append(
+            [
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+                [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1],
+                [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 1],
+                [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ]
+        )
 
     # Mountains random placement
 
@@ -143,7 +149,10 @@ class World:
 
         for _ in range(nbMountains):
 
-            x_offset, y_offset = random.randint(0, len(self.Map_mountains[0]) - 1), random.randint(0, len(self.Map_mountains) - 1)
+            x_offset, y_offset = (
+                random.randint(0, len(self.Map_mountains[0]) - 1),
+                random.randint(0, len(self.Map_mountains) - 1),
+            )
 
             M = self.MountainsType[random.randint(0, len(self.MountainsType) - 1)]
 
@@ -154,29 +163,24 @@ class World:
                     x2, y2 = x + x_offset, y + y_offset
 
                     if x2 < 0:
-
                         x2 += len(self.Map_mountains[0])
 
                     if x2 >= len(self.Map_mountains[0]):
-
                         x2 -= len(self.Map_mountains[0])
 
                     if y2 < 0:
-
                         y2 += len(self.Map_mountains)
 
                     if y2 >= len(self.Map_mountains):
-
                         y2 -= len(self.Map_mountains)
 
                     if not self.Map_mountains[y2][x2]:
-                        
                         self.Map_mountains[y2][x2] = M[y][x]
-                        
-                        block = Block(self, x2,y2)
-                        
+
+                        block = Block(self, x2, y2)
+
                         self.block_group.add(block)
-                        
+
                         self.all_object_group.add(block)
 
         # Tree and obstacle random placment
@@ -186,29 +190,29 @@ class World:
             for y in range(len(self.Map_trees)):
 
                 x_mn, y_mn = x - 1, y - 1
-                
+
                 x_mx, y_mx = x + 1, y + 1
-                
+
                 if x_mn < 0:
-                    
                     x_mn += len(self.Map_mountains[0])
-                    
+
                 if x_mx >= len(self.Map_mountains[0]):
-                    
                     x_mx -= len(self.Map_mountains[0])
-                    
+
                 if y_mn < 0:
-                    
                     y_mn += len(self.Map_mountains)
-                    
+
                 if y_mx >= len(self.Map_mountains):
-                    
                     y_mx -= len(self.Map_mountains)
 
-                if not self.Map_mountains[y][x] or (self.Map_mountains[y_mx][x] > 0 and self.Map_mountains[y_mn][x] > 0 and self.Map_mountains[y][x_mx] > 0 and self.Map_mountains[y][x_mn] > 0):
+                if not self.Map_mountains[y][x] or (
+                    self.Map_mountains[y_mx][x] > 0
+                    and self.Map_mountains[y_mn][x] > 0
+                    and self.Map_mountains[y][x_mx] > 0
+                    and self.Map_mountains[y][x_mn] > 0
+                ):
 
                     if random.random() < 0.03:
-
                         self.Map_trees[y][x] = Tree.Tree(self, x, y)
 
                         self.Trees.append((x, y))
@@ -220,8 +224,7 @@ class World:
                         continue
 
                     if random.random() < 0.01:
-
-                        obstacle = Obstacle(self,x,y)
+                        obstacle = Obstacle(self, x, y)
 
                         self.obstacle_group.add(obstacle)
 
@@ -229,12 +232,11 @@ class World:
 
                         continue
 
-                    #self.Trees.append((x, y))
+                    # self.Trees.append((x, y))
 
         # set frame block
 
         for block in self.block_group:
-
             block.update_block()
 
         # Grass random placment
@@ -245,31 +247,42 @@ class World:
 
                 grass = Grass(self, x, y)
 
-                if not pygame.sprite.spritecollideany(grass,self.all_object_group):
-                    
+                if not pygame.sprite.spritecollideany(grass, self.all_object_group):
+
                     if random.random() < 0.1:
-                        
                         self.Map_grass[y][x] = grass
-                        
+
                         self.Grass.append((x, y))
-                        
+
                         self.grass_group.add(self.Map_grass[y][x])
 
         # Générer des agents
         for i in range(10):
-            sheep = Agent.Sheep(self, (
-                random.randint(0, self.screenWidth // self.size_tile_X) * self.size_tile_X,
-                random.randint(0, self.screenHeight // self.size_tile_Y) * self.size_tile_Y))
-            collide = pygame.sprite.spritecollideany(sheep,self.all_object_group)
+            sheep = Agent.Sheep(
+                self,
+                (
+                    random.randint(0, self.screenWidth // self.size_tile_X)
+                    * self.size_tile_X,
+                    random.randint(0, self.screenHeight // self.size_tile_Y)
+                    * self.size_tile_Y,
+                ),
+            )
+            collide = pygame.sprite.spritecollideany(sheep, self.all_object_group)
             if collide:
                 sheep.kill()
-                i-=1
+                i -= 1
                 continue
             self.sheep_group.add(sheep)
         for i in range(5):
-            wolf = Agent.Wolf(self, (
-                random.randint(0, self.screenWidth // self.size_tile_X) * self.size_tile_X,
-                random.randint(0, self.screenHeight // self.size_tile_Y) * self.size_tile_Y))
+            wolf = Agent.Wolf(
+                self,
+                (
+                    random.randint(0, self.screenWidth // self.size_tile_X)
+                    * self.size_tile_X,
+                    random.randint(0, self.screenHeight // self.size_tile_Y)
+                    * self.size_tile_Y,
+                ),
+            )
             collide = pygame.sprite.spritecollideany(wolf, self.all_object_group)
             if collide:
                 wolf.kill()
@@ -277,38 +290,33 @@ class World:
                 continue
             self.wolf_group.add(wolf)
 
-        
-        #générer nuages
+        # générer nuages
 
         for _ in range(15):
             cloud = Cloud(self)
             self.cloud_group.add(cloud)
 
-    
-
     def update_object(self):
 
         # Weather
 
-        self.weather.update_weather() 
+        self.weather.update_weather()
 
         if self.weather.delay == 1:
-            print("p_fire=",Tree.P_FIRE)
-            print("p_gen=",Tree.P_REGEN)
-            print("saison=",self.weather.season)
-        
+            print("p_fire=", Tree.P_FIRE)
+            print("p_gen=", Tree.P_REGEN)
+            print("saison=", self.weather.season)
+
         # BLOCK
 
         for block in self.block_group:
-
             block.set_frame()
-        
+
         self.block_group.draw(self.screen)
 
         # TREE
 
         if not self.Tmp1:
-
             self.Tmp1 = self.Trees.copy()
 
         i = random.randint(0, len(self.Tmp1) - 1)
@@ -317,7 +325,9 @@ class World:
 
         if self.Map_trees[y][x] == None:
 
-            if random.random() < Tree.P_REGEN: #probabilté qu'un arbre repousse (change par rapport à la saison et température)
+            if (
+                random.random() < Tree.P_REGEN
+            ):  # probabilté qu'un arbre repousse (change par rapport à la saison et température)
 
                 self.Map_trees[y][x] = Tree.Tree(self, x, y)
 
@@ -345,7 +355,6 @@ class World:
         # GRASS
 
         if not self.Tmp2:
-
             self.Tmp2 = self.Grass.copy()
 
         i = random.randint(0, len(self.Tmp2) - 1)
@@ -354,7 +363,9 @@ class World:
 
         if self.Map_grass[y][x] == None:
 
-            if random.random() < 0.004: #probabilté que de l'herbe repousse (change par rapport à la saison et température)
+            if (
+                random.random() < 0.004
+            ):  # probabilté que de l'herbe repousse (change par rapport à la saison et température)
 
                 self.Map_grass[y][x] = Grass(self, x, y)
 
@@ -363,7 +374,7 @@ class World:
                 self.Map_grass[y][x].update_grass()
 
         else:
-            
+
             self.Map_grass[y][x].update_grass()
 
         self.grass_group.update()
@@ -385,36 +396,32 @@ class World:
         # CLOUD
 
         for cloud in self.cloud_group:
-
             cloud.update()
 
         self.cloud_group.update()
 
         self.cloud_group.draw(self.screen)
 
-
     def update_world(self):
         """
         lecture événementielles du jeux
 
         """
-        
 
         # lecture des événements Pygame
         for event in pygame.event.get():
             if event.type == QUIT:
                 self.destroy()
- 
+
         for x in range(self.screenWidth // self.size_bg_X + 1):
 
             for y in range(self.screenHeight // self.size_bg_Y + 1):
-                
-                self.screen.blit(self.Environment_images[0][self.weather.get_season()], (x * self.size_bg_X, y * self.size_bg_Y))
-
+                self.screen.blit(
+                    self.Environment_images[0][self.weather.get_season()],
+                    (x * self.size_bg_X, y * self.size_bg_Y),
+                )
 
         self.update_object()
-
-        
 
         pygame.display.update()
 
@@ -422,7 +429,7 @@ class World:
         """
         destructeur de la classe
         """
-        print('Exit')
+        print("Exit")
         pygame.quit()  # ferme la fenêtre principale
         exit()
 
@@ -430,6 +437,7 @@ class World:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 self.destroy()
+
 
 if __name__ == "__main__":
     world = World()
