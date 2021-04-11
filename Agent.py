462450bae82c4f0c4516414ddbc2e8b2ec3d29a3
ZhenyueFu <zhenyuef@gmail.com>
2021-04-11 21:53:21 +0200

    pause of agent

--- Agent.py
+++ Agent.py
@@ -1,6 +1,13 @@
 import random
+from abc import abstractmethod
+from enum import Enum, auto
 
 import pygame  # PYGAME package 
+
+
+class AgentState(Enum):
+    MOVE = auto()
+    PAUSE = auto()
 
 
 class BasicAgent(pygame.sprite.Sprite):
@@ -25,6 +32,9 @@ class BasicAgent(pygame.sprite.Sprite):
         self.input_direction = -1
         self.next_direction = -1
         self.it_non_mange = 0
+        self.state = AgentState.MOVE
+        self.move_time = 0
+        self.move_time_limit = random.randint(10, 40)
 
     def set_frame(self, frame):
         self.frame = frame
@@ -40,13 +50,26 @@ class BasicAgent(pygame.sprite.Sprite):
         self.y = self.rect.y // self.world.size_tile_Y
 
     def turn(self):
-        self.direction += 2
+        if self.direction == 0:
+            self.rect.y += self.speed
+        if self.direction == 1:
+            self.rect.x -= self.speed
+        if self.direction == 2:
+            self.rect.y -= self.speed
+        if self.direction == 3:
+            self.rect.x += self.speed
+        self.direction += 1
         if self.direction > 3:
             self.direction -= 4
         if self.direction < 0:
             self.direction += 4
 
+
     def move(self):
+        self.move_time += 1
+        if self.move_time >= self.move_time_limit:
+            self.state = AgentState.PAUSE
+            self.move_time = 0
         self.frame_change_counter += 1
         obstacle_list = pygame.sprite.spritecollide(self, self.world.all_object_group, False)
         if obstacle_list:
@@ -101,14 +124,26 @@ class BasicAgent(pygame.sprite.Sprite):
             if self.rect.right < 0:
                 self.rect.left = self.world_size_x
 
+    @abstractmethod
+    def reproduire(self):
+        pass
+
+    def pause(self):
+        if self.it_non_mange > 20:
+            self.state = AgentState.MOVE
+
     def update(self):
-        self.move()
+        if self.state is AgentState.MOVE:
+            self.move()
+            self.reproduire()
+        else:
+            self.pause()
         self.calc_pos()
 
 
 class Sheep(BasicAgent):
-    p_reproduce = 0.0015
-    delai_de_famine = 10
+    p_reproduce = 0.00015
+    delai_de_famine = 100
 
     def __init__(self, world, init_pos):
         super().__init__(world, world.sheep_images[0][0], init_pos)
@@ -120,24 +155,24 @@ class Sheep(BasicAgent):
             agent = Sheep(self.world, self.rect.topleft)
             agent.add(self.world.sheep_group)
 
-
     def update(self):
         super().update()
-        self.reproduire()
+
         pos = pygame.math.Vector2(self.x, self.y)
-        if self.iter > 10:
+        if self.iter > 3:
             self.it_non_mange += 1
             self.iter = 0
         min = 6
-        cloest = None
+        closest = None
         for wolf in self.world.wolf_group:
             distance = pos.distance_to(pygame.math.Vector2(wolf.x, wolf.y))
             if min > distance:
                 min = distance
-                cloest = wolf
-        if cloest:
-            x_distance = cloest.x - self.x
-            y_distance = cloest.y - self.y
+                closest = wolf
+        if closest:
+            self.state= AgentState.MOVE
+            x_distance = closest.x - self.x
+            y_distance = closest.y - self.y
             if abs(x_distance) > abs(y_distance):
                 if y_distance > 0:
                     self.set_direction(0)
@@ -152,12 +187,12 @@ class Sheep(BasicAgent):
                 grass.kill()
                 self.world.Map_grass[grass.y][grass.x] = None
                 self.reset_mange()
-        if self.it_non_mange > Wolf.delai_de_famine:
+        if self.it_non_mange > Sheep.delai_de_famine:
             self.kill()
 
 
 class Wolf(BasicAgent):
-    p_reproduce = 0.001
+    p_reproduce = 0.0001
     delai_de_famine = 100
 
     def __init__(self, world, init_pos):
@@ -173,14 +208,13 @@ class Wolf(BasicAgent):
 
     def update(self):
         super().update()
-        self.reproduire()
         pos = pygame.math.Vector2(self.x, self.y)
         self.iter += 1
         if self.iter > 10:
             self.it_non_mange += 1
             self.iter = 0
         min = 5
-        cloest = None
+        closest = None
         for sheep in self.world.sheep_group:
             if pygame.sprite.collide_rect(self, sheep):
                 sheep.kill()
@@ -189,10 +223,11 @@ class Wolf(BasicAgent):
             distance = pos.distance_to(pygame.math.Vector2(sheep.x, sheep.y))
             if min > distance:
                 min = distance
-                cloest = sheep
-        if cloest:
-            x_distance = cloest.x - self.x
-            y_distance = cloest.y - self.y
+                closest = sheep
+        if closest:
+            self.state = AgentState.MOVE
+            x_distance = closest.x - self.x
+            y_distance = closest.y - self.y
             if abs(x_distance) < abs(y_distance):
                 if y_distance > 0:
                     self.set_direction(2)
