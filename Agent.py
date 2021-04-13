6cdbc138895e3db1b9b7faf82fb29c70be362507
ZhenyueFu <zhenyuef@gmail.com>
2021-04-14 00:49:51 +0200

    Modification des mouvements des agents

--- Agent.py
+++ Agent.py
@@ -19,12 +19,14 @@ class BasicAgent(pygame.sprite.Sprite):
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.topleft = init_pos
+        self.last_x = self.rect.x
+        self.last_y = self.rect.y
         self.speed = 2
         self.frame = []
         self.current_frame = 0
         self.x = self.rect.x // self.world.size_tile_X
         self.y = self.rect.y // self.world.size_tile_Y
-        self.direction = 3
+        self.direction = random.randint(0,3)
         self.alive = True
         self.frame_change_counter = 0
         self.direction_change_counter = 0
@@ -34,7 +36,8 @@ class BasicAgent(pygame.sprite.Sprite):
         self.it_non_mange = 0
         self.state = AgentState.MOVE
         self.move_time = 0
-        self.move_time_limit = random.randint(10, 40)
+        self.move_time_limit = random.randint(30, 70)
+        self.obstacle_direction = -1
 
     def set_frame(self, frame):
         self.frame = frame
@@ -43,92 +46,97 @@ class BasicAgent(pygame.sprite.Sprite):
         self.it_non_mange = 0
 
     def set_direction(self, direction):
-        self.input_direction = direction
+        if direction != self.obstacle_direction:
+            self.next_direction = direction
 
     def calc_pos(self):
         self.x = self.rect.x // self.world.size_tile_X
         self.y = self.rect.y // self.world.size_tile_Y
 
-    def turn(self):
-        if self.direction == 0:
-            self.rect.y += self.speed
-        if self.direction == 1:
-            self.rect.x -= self.speed
-        if self.direction == 2:
-            self.rect.y -= self.speed
-        if self.direction == 3:
-            self.rect.x += self.speed
-        self.direction += 1
-        if self.direction > 3:
-            self.direction -= 4
-        if self.direction < 0:
-            self.direction += 4
+    def rand_direction(self,curr_direction):
+        d = random.randint(0,3)
+        while d == curr_direction:
+            d = random.randint(0,3)
+        return d
 
+    def turn(self):
+        print(self.rect.x)
+        self.rect.x = self.last_x
+        print(self.rect.x)
+        self.rect.y = self.last_y
+        self.obstacle_direction = self.direction
+        self.direction = self.rand_direction(self.direction)
+
+    def collide(self):
+        obstacle = pygame.sprite.spritecollideany(self,self.world.all_object_group,pygame.sprite.collide_rect_ratio(0.7))
+        if obstacle:
+            self.turn()
 
     def move(self):
         self.move_time += 1
-        if self.move_time >= self.move_time_limit:
+        if self.move_time >= self.move_time_limit and self.it_non_mange < 20:
             self.state = AgentState.PAUSE
             self.move_time = 0
         self.frame_change_counter += 1
-        obstacle_list = pygame.sprite.spritecollide(self, self.world.all_object_group, False)
-        if obstacle_list:
-            self.turn()
         if self.frame_change_counter > 3:
             self.current_frame += 1
             if self.current_frame > 2:
                 self.current_frame = 0
             self.frame_change_counter = 0
-            if self.input_direction != -1:
-                self.next_direction = self.input_direction
-                self.input_direction = -1
         self.image = self.frame[self.direction][self.current_frame]
 
         self.direction_change_counter += 1
         if self.direction_change_counter > self.direction_change_time:
             self.direction_change_counter = 0
+            self.obstacle_direction = -1
             self.direction_change_time = random.randint(30, 150)
             if self.next_direction == -1:
-                if random.random() > 0.5:
-                    self.next_direction = (self.direction + 1) % 4
-                else:
-                    self.next_direction = (self.direction - 1 + 4) % 4
+                self.rand_direction(self.direction)
 
-        if self.next_direction != -1 and self.rect.top % self.world.size_tile_X == 0 and self.rect.left % self.world.size_tile_Y == 0:
+        if self.next_direction != -1:
             self.direction = self.next_direction
             self.next_direction = -1
+        if self.direction == self.obstacle_direction:
+            self.direction = self.rand_direction(self.direction)
+        self.last_x = self.rect.x
+        self.last_y = self.rect.y
         # [0] -> up
         # [1] -> right
         # [2] -> down
         # [3] -> left
         if self.direction == 0:
             self.rect.y -= self.speed
-            if self.rect.bottom < 0:
-                self.rect.top = self.world_size_y
+            self.collide()
+            if self.rect.top < 0:
+                self.rect.bottom = self.world_size_y
             return
 
         if self.direction == 1:
             self.rect.x += self.speed
-            if self.rect.left > self.world_size_x:
+            self.collide()
+            if self.rect.right > self.world_size_x:
                 self.rect.right = 0
             return
 
         if self.direction == 2:
             self.rect.y += self.speed
-            if self.rect.top > self.world_size_y:
-                self.rect.bottom = 0
+            self.collide()
+            if self.rect.bottom > self.world_size_y:
+                self.rect.top = 0
             return
 
         if self.direction == 3:
             self.rect.x -= self.speed
-            if self.rect.right < 0:
-                self.rect.left = self.world_size_x
+            self.collide()
+            if self.rect.left < 0:
+                self.rect.right = self.world_size_x
 
     @abstractmethod
     def reproduire(self):
         pass
 
     def pause(self):
+        self.image = self.frame[2][0]
         if self.it_non_mange > 20:
             self.state = AgentState.MOVE
 
@@ -162,7 +170,7 @@ class Sheep(BasicAgent):
         if self.iter > 3:
             self.it_non_mange += 1
             self.iter = 0
-        min = 20
+        min = 10
         closest = None
         for wolf in self.world.wolf_group:
             distance = pos.distance_to(pygame.math.Vector2(wolf.x, wolf.y))
@@ -182,7 +190,7 @@ class Sheep(BasicAgent):
                 self.set_direction(3)
             else:
                 self.set_direction(1)
-        for grass in self.world.grass_group:
+        for grass in self.world.grass_mature_group:
             if pygame.sprite.collide_rect(self, grass):
                 grass.kill()
                 self.world.Map_grass[grass.y][grass.x] = None
@@ -213,7 +221,7 @@ class Wolf(BasicAgent):
         if self.iter > 10:
             self.it_non_mange += 1
             self.iter = 0
-        min = 15
+        min = 5
         closest = None
         for sheep in self.world.sheep_group:
             if pygame.sprite.collide_rect(self, sheep):
