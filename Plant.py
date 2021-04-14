7cf9498a641a24941cdea60eef9c416ed90a2d38
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-04-14 13:10:37 +0200

    new

--- Plant.py
+++ Plant.py
@@ -228,10 +228,11 @@ class Grass(Plant):
     def set_frame(self):
         if self.world.weather.season == 2:
             self.frame=self.world.Environment_images[6]
+            self.image=self.frame[self.state]
         else:
             self.frame=self.world.Environment_images[2]
+            self.image=self.frame[self.state]
 
     # Maj herbe
     def update(self):
         super().update_plant()
-        self.set_frame()
